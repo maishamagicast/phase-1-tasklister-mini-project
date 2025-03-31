6882c070a287e5b28d02c43980156f83daaaeaf9
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-03-31 21:27:42 +0300

    preventDefault()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputValue= document.getElementById('new-task-description');
+  const inputDisplay= document.getElementById('tasks');
+  const form= document.getElementById('create-task-form');
+
+  form.addEventListener('submit',function(event){
+    event.preventDefault();
+    
+    const newElement= document.createElement('li');
+    newElement.textContent=inputValue.value;
+    inputDisplay.append(newElement);
+    
+    
+  })
 });
